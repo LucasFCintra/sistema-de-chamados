b7543318b4709878598d1e34f108f2ff727faef5
LucasFCintra <lucasfmcintra12@gmail.com>
2021-09-09 00:57:21 +0000

    Criação de tela de login e cadastro

--- sistema-de-chamados/src/pages/SignIn/index.js
+++ sistema-de-chamados/src/pages/SignIn/index.js
@@ -1,9 +1,39 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './signin.css';
+import logo from '../../assets/logo.png';
+import Button from '@material-ui/core/Button';
 
 function SignIn() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    alert('Cliclou')
+  }
+
     return (
-      <div>
-        <h1> Página de login </h1>
+      <div className="container-center">
+        <div className="login">
+
+          <div className="logo-area">
+            <img src={logo} alt="Logo" />
+          </div>
+
+          <form onSubmit={handleSubmit}>
+
+            <h1>Entrar</h1>
+            <input type="text" placeholder="email@email.com" value={email} onChange={ (e) => setEmail(e.target.email)} />
+            <input type="password" placeholder="********" value={password} onchange={ (e) => setPassword(e.target.value)} />
+            <Button variant="contained" color="primary" type="submit">Primary</Button>
+
+          </form>
+
+          <Link to="/register">Criar uma conta</Link>
+
+        </div>
       </div>
     );
   }
--- sistema-de-chamados/src/pages/SignUp/index.js
+++ sistema-de-chamados/src/pages/SignUp/index.js
@@ -1,8 +1,41 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
+import Button from '@material-ui/core/Button';
 
 function SignUp() {
+
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    alert('Cliclou')
+  }
+
     return (
-      <div>
-        <h1> Página de cadastro </h1>
+      <div className="container-center">
+        <div className="login">
+
+          <div className="logo-area">
+            <img src={logo} alt="Logo" />
+          </div>
+
+          <form onSubmit={handleSubmit}>
+
+            <h1>Entrar</h1>
+            <input type="text" placeholder="Seu nome" value={nome} onChange={ (e) => setNome(e.target.nome)} />
+            <input type="text" placeholder="email@email.com" value={email} onChange={ (e) => setEmail(e.target.email)} />
+            <input type="password" placeholder="********" value={password} onchange={ (e) => setPassword(e.target.value)} />
+            
+            <Button variant="contained" color="primary" type="submit">Cadastrar</Button>
+
+          </form>
+
+          <Link to="/">Já possuo uma conta</Link>
+
+        </div>
       </div>
     );
   }
